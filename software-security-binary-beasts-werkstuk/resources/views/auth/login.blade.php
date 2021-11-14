e7894ba0555ffcdab0e5eb2daea241f28a242a73
Ali Amsalkhir <AmsalkhirAli@gmail.com>
2021-11-14 18:13:08 +0100

    Add link to register from login screen

--- software-security-binary-beasts-werkstuk/resources/views/auth/login.blade.php
+++ software-security-binary-beasts-werkstuk/resources/views/auth/login.blade.php
@@ -32,7 +32,9 @@
                 </label>
             </div>
 
+
             <div class="flex items-center justify-end mt-4">
+
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
@@ -42,6 +44,12 @@
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
+            </div>
+
+            <div class="flex items-center justify-center mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                {{ __('Not yet registered? create account here') }}
+            </a>
             </div>
 
 
