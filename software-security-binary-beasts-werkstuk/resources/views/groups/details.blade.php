f6d3209a52778b5e49dc5bde90443b4cb5d10558
Kenny Passenier <kennypassenier@gmail.com>
2021-11-25 21:57:45 +0100

    Group details

--- software-security-binary-beasts-werkstuk/resources/views/groups/details.blade.php
+++ software-security-binary-beasts-werkstuk/resources/views/groups/details.blade.php
@@ -1,12 +1,102 @@
 <x-app-layout>
 
+  @if(Auth::user()->roles()->where("role_name", "TEACHER")->count() == 1)
   <div class="flex flex-col">
     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
       <livewire:users-table :group='$group'>
     </div>
   </div>
+  @endif
 
-  
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" colspan=2
+                  class="px-6 py-3 text-center text-xl font-medium text-gray-500 uppercase tracking-wider">
+                  Group members
+                </th>
+              </tr>
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Email
+                </th>
+              </tr>
+            </thead>
+            <br>
+            @foreach($group->members as $member)
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">
+                  {{ $member->name }}
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">{{ $member->email }}</div>
+              </td>
+            </tr>
+            @endforeach
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  @if($group->events()->count() > 0)
+
+  <div class="py-12">
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th colspan=2 scope="col"
+                  class="px-6 py-3 text-center text-xl font-medium text-gray-500 uppercase tracking-wider">
+                  EVENTS for group {{ $group->name }}
+
+                </th>
+              </tr>
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Title
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Date
+                </th>
+              </tr>
+            </thead>
+            @foreach($group->events as $event)
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">
+                  <a href="{{url('events/') . '/' . $event->id}}">
+                    {{ $event->eventname }}
+
+                  </a>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">{{ $event->eventdate }} - {{ $event->eventtime}}</div>
+              </td>
+            </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endif
+
 
 
 
