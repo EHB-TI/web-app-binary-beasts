f8fcb1ee76c8f1a2ef34d92e6d20f6a4dc9e6351
Kenny Passenier <kennypassenier@gmail.com>
2021-11-25 02:13:19 +0100

    Dashboard change

--- software-security-binary-beasts-werkstuk/resources/views/dashboard.blade.php
+++ software-security-binary-beasts-werkstuk/resources/views/dashboard.blade.php
@@ -1,15 +1,59 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center">
+        <h1>Welcome to the homepage for Binary Beasts.</h1>
+        <h2>This website allows users to manage events and groups.</h2>
+        <br>
+        <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae eligendi, sunt
+          repudiandae
+          distinctio impedit et. A quia expedita, nulla nesciunt earum ad eaque sint commodi dolor accusamus nihil quos.
+          Nihil distinctio ut vitae suscipit animi, ipsa numquam! Iste consequatur tempora harum voluptate. Laudantium
+          quae, sunt totam, quis odio eligendi ratione tempora ipsam libero non hic laborum iusto. Cum, facilis.
+          Aliquid ratione quis possimus, rem quam architecto corporis facere dignissimos est quasi commodi explicabo
+          nihil dolor aperiam. Magnam laborum amet, cupiditate quia excepturi doloremque, odio adipisci accusantium
+          provident, nam consectetur!
+          Nisi reprehenderit perferendis error ipsum molestiae doloremque quaerat! Vitae quae doloremque hic modi
+          nostrum quaerat est rerum odit id! Accusantium minus magni reprehenderit tempore alias vitae quibusdam aut
+          laborum placeat!
+          Debitis quam quibusdam fuga ut doloribus dolorem molestias unde adipisci tempora ad, recusandae iste velit
+          architecto sint, fugiat itaque ullam vel at consectetur minima tempore repellendus officia iure nisi? Unde!
+          Facere deserunt itaque alias quisquam doloribus magni? Tempora dolor vel voluptatibus temporibus nemo fugit
+          quia incidunt, ab recusandae facere consequuntur impedit qui possimus. Placeat rem ad eos deserunt inventore
+          laudantium?
+          Fugit, voluptatibus sed laborum, deleniti libero a debitis reiciendis vero odit suscipit reprehenderit culpa
+          aperiam ipsam aut saepe tenetur! Aut voluptatem, consequatur error vitae temporibus ratione minus fugit
+          officia aperiam!
+          Incidunt exercitationem veritatis ex ad hic atque minima, magnam aut non! Praesentium, iure nesciunt quidem
+          alias officia odit non vero dolorum ullam ea eaque beatae quibusdam odio minima maxime illo.
+          Neque aliquam accusamus velit dignissimos ducimus ipsa dicta ad fuga tenetur quia, optio nobis, animi
+          laudantium libero assumenda voluptate rem corrupti saepe, dolorum accusantium? Numquam est possimus hic sed
+          expedita?
+          Qui, consectetur reiciendis? Debitis quod incidunt vero quidem voluptatem tempora animi officia deleniti
+          accusamus eaque rerum suscipit in, assumenda rem corrupti doloribus culpa, molestiae a. Consequuntur dolorem
+          possimus ex atque!
+          Totam quibusdam voluptatum fuga reprehenderit maxime cupiditate harum temporibus laudantium vitae ipsam
+          tempora in ab, voluptas magni ea qui illo consectetur nisi quidem magnam perferendis voluptatem. Sequi, ab
+          excepturi? Ad?
+          Placeat enim illum assumenda neque consectetur ipsum beatae reprehenderit eum dolore aspernatur. Rem enim
+          optio tempore ut, doloremque veniam odit architecto nulla non aut consequuntur quas! Ullam fugiat cum
+          repellendus?
+          Veniam debitis ea totam voluptatibus expedita! Rerum, nostrum, recusandae saepe eligendi soluta possimus
+          accusantium enim, doloribus repellendus impedit inventore adipisci pariatur autem. Maiores nihil
+          exercitationem, similique quia doloremque reiciendis veritatis!
+          Hic soluta ab dolor consectetur eum adipisci possimus voluptatem pariatur labore alias facilis et, harum
+          assumenda illo aspernatur magnam voluptatibus quod quidem doloribus fuga velit est. Doloribus ad incidunt
+          odio.
+          Eligendi non architecto a, cumque atque perspiciatis facilis reiciendis ullam nemo velit neque laborum rerum
+          placeat molestias impedit quos dolor libero vel possimus iste omnis quibusdam voluptatibus. Ea, quia
+          consequatur.</p>
+      </div>
     </div>
+  </div>
 </x-app-layout>
